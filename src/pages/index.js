f0a831449deec833f0e3d824b04641c560a4b314
Hiram Munoz <hirammunozr@outlook.com>
2022-02-17 16:35:07 -0600

    Added change of searchField contents if hash changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,59 @@
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import queryString from "query-string";
 import {
-  Collapse,
-  Grid,
-  Typography,
-  Box,
-  TextField,
-  Grow,
-  Stack,
-  useTheme
+  Collapse, Grid, Typography, Box, TextField, Grow, Stack, useTheme
 } from "@mui/material";
 import EquationCard from "../components/equationCard";
 
 // Temporary import to test links
 import { graphql } from "gatsby";
 
-const test_content = [
-  {
-    title: "Sigmoid Function",
-    category: "Mathematical Functions",
-    latex: "S(x)=\\frac{1}{1+e^{-x}}"
-  }, {
-    title: "Law of Universal Gravitation",
-    category: "Classical Mechanics",
-    latex: "F=G\\frac{m_1m_2}{r^2}"
-  }, {
-    title: "Newton's Second Law of Motion",
-    category: "Classical Mechanics",
-    latex: "\\bm{F}_{net} = m\\bm{a}"
-  }, {
-    title: "Dot product",
-    category: "Algebra",
-    latex: "\\bm{a}\\cdot\\bm{b}=\\sum^n_{i=1}a_ib_i"
-  }];
-
 function IndexPage({
   navigate,
   location: { hash },
   data: { allMarkdownRemark: { nodes } }
-}) {
-  // Hooks
+}, ...rest) {
+
+  //region Hooks
   // Use current location's hash as initial search field contents, if present.
   const [searchField, setSearchField] = useState(
     queryString.parse(hash).search ?? "");
-  // If entered page with an initial search, set search active immediately.
-  const [isSearchActive, setSearchActive] = useState(!!searchField);
   const theme = useTheme();
+  // Add listener for URL hash to detect if search field changes.
+  useEffect(() => {
+    const handleHashChange = (event) => {
+      const { search } = queryString.parse(event.target.location.hash);
 
-  const searchFieldTransition = theme.transitions.create(["margin"]);
+      setSearchField(search ?? "")
+    };
+    window.addEventListener("hashchange", handleHashChange);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+  //endregion
 
-  // Handler functions
+  //region Handlers
   const handleSearchFieldChange = (event) => {
     setSearchField(event.target.value);
   };
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
-      setSearchActive(true);
-      const query = queryString.stringify({
+      // Add hash search field only if it's not an empty string.
+      let path = !!searchField ? `#${queryString.stringify({
         search: searchField
-      });
-      navigate(`/#${query}`);
-      event.preventDefault();
+      })}` : "";
+      navigate(path);
     }
   };
+  //endregion
+
+  //region Constants
+  const currentSearch = queryString.parse(hash).search ?? "";
+  const isSearchActive = !!currentSearch
+  const searchFieldTransition = theme.transitions.create(["margin"]);
+  //endregion
 
   return <Stack sx={{
     height: "100%",
@@ -91,17 +81,16 @@ function IndexPage({
                  gutterBottom autoComplete="off" autoFocus />
     </Box>
     <Grid container spacing={2} padding={theme.spacing(4, 2)}>
-      {isSearchActive ? nodes.map(
-        (document, index) => {
-          const { frontmatter } = document;
-          return <Grid item xs={12} s={6} md={6} lg={4}>
-            <Grow in={isSearchActive} timeout={750}>
-              <EquationCard key={index} title={frontmatter.title}
-                            category="what" slug={frontmatter.slugra}
-                            latex={frontmatter.latex} variant="outlined" />
-            </Grow>
-          </Grid>;
-        }) : null}
+      {isSearchActive ? nodes.map((document, index) => {
+        const { frontmatter } = document;
+        return <Grid item xs={12} s={6} md={6} lg={4}>
+          <Grow in={isSearchActive} timeout={750}>
+            <EquationCard key={index} title={frontmatter.title}
+                          category="what" slug={frontmatter.slugra}
+                          latex={frontmatter.latex} variant="outlined" />
+          </Grow>
+        </Grid>;
+      }) : null}
     </Grid>
   </Stack>;
 }
